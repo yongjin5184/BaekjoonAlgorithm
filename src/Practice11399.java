a73f9ace2f1d2afd5628c60ece22361f7cd4a5c6
Kimyongjin <yongjin5184@gmail.com>
2018-09-25 14:24:00 +0900

    Rename

--- src/Practice11399.java
+++ src/Practice11399.java
@@ -9,13 +9,13 @@ import java.util.*;
  */
 
 
-public class Practice19 {
+public class Practice11399 {
 
 
     public static void main(String[] args) throws IOException{
 
         /*
-        *  문제 : ATM (백준 11399번)
+        *  문제 : ATM (백준 11399번) - Greedy
         *  입력 : 첫째 줄에 사람의 수 N(1 ≤ N ≤ 1,000)이 주어진다.
         *        둘째 줄에는 각 사람이 돈을 인출하는데 걸리는 시간 Pi가 주어진다. (1 ≤ Pi ≤ 1,000)
         *  출력 : 첫째 줄에 각 사람이 돈을 인출하는데 필요한 시간의 합의 최소값을 출력한다.
@@ -27,15 +27,6 @@ public class Practice19 {
         String[] arr;
 
         arr = br.readLine().split(" ");
-
-        /*
-        for(int i = 0; i < N; i++){
-            System.out.println("");
-            for(int j = 0; j <= i; j++){
-                System.out.print("arr[" + j + "] = " + arr[j]);
-            }
-        }
-        */
         Arrays.sort(arr, (num1, num2) -> Integer.parseInt(num1) - Integer.parseInt(num2));
 //        Arrays.sort(arr);
 
@@ -47,7 +38,6 @@ public class Practice19 {
                 sum += Integer.parseInt(arr[j]);
             }
         }
-
 
         System.out.print(sum);
     }
