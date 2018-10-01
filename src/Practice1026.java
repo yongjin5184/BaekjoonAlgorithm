4eb61c8d666f7933395c0eff6d694ee3b31ae6fe
Kimyongjin <yongjin5184@gmail.com>
2018-10-01 10:56:54 +0900

    백준 1026번 보물

--- src/Practice1026.java
+++ src/Practice1026.java
@@ -1,5 +1,3 @@
-package src.src;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,12 +8,37 @@ import java.util.Arrays;
  */
 public class Practice1026 {
     public static void main(String[] args) throws IOException{
+        /**
+         * @description : 백준 1026번 보물
+         * @solution : 1. 첫 번째 배열은 asc 정렬
+         *             2. 두 번째 배열은 desc 정렬
+         *             3. 두 정렬된 배열을 돌면서 원소들을 곱해서 sum한 값을 출력한다.
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String line = "";
+        String line;
+        int index = 0;
+        int[] arr1 = new int[N];
+        String[] arr2 = new String[N];
         while((line = br.readLine()) != null && !line.equals("")){
             String[] strs = line.split(" ");
-            Arrays.stream(strs).mapToInt(Integer::parseInt).toArray();
+
+            if(index == 0) {
+                arr1 = Arrays.stream(strs).mapToInt(Integer::parseInt).toArray();
+                Arrays.sort(arr1);
+            }else{
+                arr2 = Arrays.copyOf(strs, N);
+                Arrays.sort(arr2, (num1, num2) -> Integer.parseInt(num2) - Integer.parseInt(num1));
+            }
+
+            index++;
         }
+
+        int sum = 0;
+        for(int i = 0; i < N; i++){
+            sum += arr1[i] * Integer.parseInt(arr2[i]);
+        }
+
+        System.out.println(sum);
     }
 }
