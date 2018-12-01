d4508c387de7455f0854b6206491dc8d86b69a52
Kimyongjin <yongjin5184@gmail.com>
2018-12-01 11:16:37 +0900

    백준 10164번 격자상의 경로

--- src/Practice10164.java
+++ src/Practice10164.java
@@ -30,7 +30,7 @@ public class Practice10164 {
         map[0][1] = 1;
         for(int i = 1; i <= N; i++){
             for(int j = 1; j <= M; j++){
-                map[i][j] = map[i - 1][j] + map[i][j-1];
+                map[i][j] = map[i - 1][j] + map[i][j - 1];
             }
         }
 
@@ -44,5 +44,4 @@ public class Practice10164 {
             System.out.println(map[ax][ay] * map[bx][by]);
         }
     }
-
 }
