4daf5f5e97f7a678fb3b6b9c9ea6720394232ac8
Kimyongjin <yongjin5184@gmail.com>
2018-12-03 08:59:55 +0900

    ATM (백준 11399번) - Greedy

--- src/Practice11399.java
+++ src/Practice11399.java
@@ -13,14 +13,12 @@ public class Practice11399 {
 
 
     public static void main(String[] args) throws IOException{
-
         /*
         *  문제 : ATM (백준 11399번) - Greedy
         *  입력 : 첫째 줄에 사람의 수 N(1 ≤ N ≤ 1,000)이 주어진다.
         *        둘째 줄에는 각 사람이 돈을 인출하는데 걸리는 시간 Pi가 주어진다. (1 ≤ Pi ≤ 1,000)
         *  출력 : 첫째 줄에 각 사람이 돈을 인출하는데 필요한 시간의 합의 최소값을 출력한다.
         * */
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
