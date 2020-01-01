7f46c29324366a095c23967cbdeae496d63d6e7e
김용진 <yongjin5184@gmail.com>
2020-01-01 11:17:27 +0900

    백준 알고리즘
    1.17609번 회문

--- src/Practice17609.java
+++ src/Practice17609.java
@@ -14,37 +14,33 @@ public class Practice17609 {
          */
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
         N = Integer.parseInt(br.readLine());
-
-        int[] resultArr = new int[N];
         for(int i = 0; i < N; i++){
             chars = br.readLine().toCharArray();
             System.out.println(getAns());
         }
     }
 
-    static Boolean isPalindrome()
-    {
-        int left=0,right=chars.length-1;
-        while(left<=right)
-        {
-            if(chars[left++]!=chars[right--]) return false;
+    private static Boolean isPalindrome() {
+        int left = 0;
+        int right = chars.length-1;
+        while(left <= right) {
+            if(chars[left++] != chars[right--]) {
+                return false;
+            }
         }
         return true;
     }
 
-    static Boolean canPalindrome(int left,int right,Boolean flag)
-    {
-        if(left>right) return true;
-        if(chars[left]==chars[right]) return canPalindrome(left+1,right-1,flag);
-        else if(flag) return canPalindrome(left + 1, right, false)
-            || canPalindrome(left, right - 1, false);
+    private static Boolean canPalindrome(int left, int right, Boolean flag) {
+        if(left > right) return true;
+        if(chars[left] == chars[right]) return canPalindrome(left + 1,right - 1, flag);
+        else if(flag) return canPalindrome(left + 1, right, false) || canPalindrome(left, right - 1, false);
         else return false;
     }
 
-    static int getAns()
-    {
+    private static int getAns() {
         if(isPalindrome()) return 0;
-        else if(canPalindrome(0,chars.length-1,true)) return 1;
+        else if(canPalindrome(0,chars.length - 1,true)) return 1;
         else return 2;
     }
 }
