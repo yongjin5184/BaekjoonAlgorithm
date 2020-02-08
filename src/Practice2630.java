4ae68ac39aeb2f4b8ef8ab29acc264c87ae7c6ef
김용진 <yongjin5184@gmail.com>
2020-02-09 08:21:08 +0900

    백준 알고리즘
    1. 2630번 색종이 만들기 (쿼드트리)

--- src/Practice2630.java
+++ src/Practice2630.java
@@ -1,12 +1,19 @@
 import java.io.*;
 
 public class Practice2630 {
-
     private static boolean[][] board;
     private static int white;
     private static int blue;
 
     public static void main(String[] args) throws Exception {
+        /**
+         * description : 백준 2630번 색종이 만들기 (쿼드트리)
+         * solution : 1. 색종이를 4조각으로 나눈다.
+         *            2. 탈출 조건은 아래와 같다.
+         *               1)색종이의 크기가 1인 경우
+         *               2)색종이의 색깔이 모두 같은 경우
+     *                3. 이때, blue, white 색종이의 갯수를 구한다.
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
         int n = Integer.parseInt(br.readLine());
 
@@ -26,30 +33,12 @@ public class Practice2630 {
 
     private static void divide(int x, int y, int n) {
         if (n == 1) {
-            if (board[x][y]) {
-                blue++;
-            } else {
-                white++;
-            }
+            countColor(board[x][y]);
             return;
         }
-        boolean sameColor = true;
-        boolean color = board[x][y];
-        out:
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[x + i][y + j] != color) {
-                    sameColor = false;
-                    break out;
-                }
-            }
-        }
-        if (sameColor) {
-            if (board[x][y]) {
-                blue++;
-            } else {
-                white++;
-            }
+
+        if (sameColor(x, y, n)) {
+            countColor(board[x][y]);
             return;
         }
 
@@ -58,6 +47,24 @@ public class Practice2630 {
         divide(x + newN, y, newN);
         divide(x, y + newN, newN);
         divide(x + newN, y + newN, newN);
+    }
+
+    private static boolean sameColor(int x, int y, int n) {
+        boolean color = board[x][y];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[x + i][y + j] != color) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static void countColor(boolean boardValue) {
+        if (boardValue) blue++;
+        else white++;
     }
 }
 
