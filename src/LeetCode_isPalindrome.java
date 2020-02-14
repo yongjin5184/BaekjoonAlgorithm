2cbaba55debee1917c2b020f443586f240cf158a
김용진 <yongjin5184@gmail.com>
2020-02-14 12:33:27 +0900

    릿코드
    1. 릿코드 isPalindrome

---
+++ src/LeetCode_isPalindrome.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LeetCode_isPalindrome {
+    public static void main(String[] args) throws IOException {
+        /**
+         * description  : LeetCode isPalindrome
+         * solution     : 기본 숫자를 reverse 하여 비교한다.
+         * result       : O(n^2)
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
+        System.out.println(isPalindrome(Integer.parseInt(br.readLine())));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+
+        int sum = 0;
+        int target = x;
+        while (x > 0) {
+            int temp = x % 10;
+            sum = sum * 10 + temp;
+            x = x / 10;
+        }
+
+        return sum == target;
+    }
+}
