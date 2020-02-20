17676117c9d667c2612ac181477153256304a328
김용진 <yongjin5184@gmail.com>
2020-02-21 00:25:01 +0900

    릿코드
    1. 릿코드 Remove Duplicates from Sorted Array

--- src/LeetCodeRemoveDuplicatesFromSortedArray.java
+++ src/LeetCodeRemoveDuplicatesFromSortedArray.java
@@ -6,6 +6,12 @@ import java.util.Arrays;
 
 public class LeetCodeRemoveDuplicatesFromSortedArray {
     public static void main(String[] args) throws IOException {
+        /**
+         * description  : LeetCode Remove Duplicates from Sorted Array
+         *                정렬된 배열에서 중복되는 숫자는 제거하고 갯수를 구해서 리턴하는 문제
+         * solution     : 1. 배열을 돌면서 큰 숫자가 나오면 그 때  count++ 하여 return
+         * result       : O(n)
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
         String[] test1 = {"0","0","1","1","1","2","2","3","3","4"};
         String[] test2 = {"1","1","2"};
