4b2d9a8766fadd7f1edceccf19ed30551f84bdca
김용진 <yongjin5184@gmail.com>
2020-02-23 08:54:17 +0900

    릿코드
    1. 릿코드 ImplementStrStr

---
+++ src/LeetCode_ImplementStrStr.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LeetCode_ImplementStrStr {
+    public static void main(String[] args) throws IOException {
+        /**
+         * description  : LeetCode Implement StrStr
+         * solution     : haystack 에서 needle 의 처음 index 를 반환한다.
+         * result       : O(n)
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
+        System.out.println(strStr(br.readLine(), br.readLine()));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
