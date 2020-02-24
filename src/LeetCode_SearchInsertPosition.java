e217af065ab26297b33d1a81f3fa3b68d6850533
김용진 <yongjin5184@gmail.com>
2020-02-24 21:45:48 +0900

    릿코드
    1. 릿코드 Search Insert Position

---
+++ src/LeetCode_SearchInsertPosition.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class LeetCode_SearchInsertPosition {
+    public static void main(String[] args) throws IOException {
+        /**
+         * description  : LeetCode Implement Search Insert Position
+         * solution     : 배열에서 target이  insert 될 위치를 찾는 문제
+         * result       : O(n)
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
+        int[] nums = Arrays.asList(br.readLine().split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+        System.out.println(searchInsert(nums, Integer.parseInt(br.readLine())));
+    }
+
+    static public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if(target <= nums[i]){
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
