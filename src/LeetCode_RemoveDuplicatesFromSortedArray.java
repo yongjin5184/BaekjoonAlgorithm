7f2d2842f22fc33fbfc4fd12f38e5d33d1b3ecda
김용진 <yongjin5184@gmail.com>
2020-03-10 08:46:19 +0900

    릿코드
    1. RemoveDuplicatesFromSortedArray 이름 변경

--- src/LeetCode_RemoveDuplicatesFromSortedArray.java
+++ src/LeetCode_RemoveDuplicatesFromSortedArray.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class LeetCodeRemoveDuplicatesFromSortedArray {
+public class LeetCode_RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) throws IOException {
         /**
          * description  : LeetCode Remove Duplicates from Sorted Array
