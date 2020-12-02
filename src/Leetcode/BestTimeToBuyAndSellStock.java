d8deee43522a2956ce14a59b30d9311bb537063d
Kimyongjin <yongjin5184@gmail.com>
2020-12-02 22:51:01 +0900

    Leetcode 121. Best Time to Buy and Sell Stock

---
+++ src/Leetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+package src.src.Leetcode;
+
+/**
+ * description: Leetcode 121. Best Time to Buy and Sell Stock
+ */
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int [] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if(prices[j] - prices[i] > maxProfit){
+                    maxProfit = prices[j] - prices[i];
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
