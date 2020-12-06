5aebc783cc796e893405ce3a42d1cffdd3f42ef1
Kimyongjin <yongjin5184@gmail.com>
2020-12-06 09:21:16 +0900

    Programmers 3진법 뒤집기
    코딩 테스트 연습 > 월간 코드 챌린지 시즌1 > 3진법 뒤집기

---
+++ src/programmers/ReversTernaryNotation.java
@@ -0,0 +1,26 @@
+package src.src.programmers;
+
+public class ReversTernaryNotation {
+
+    public static int solution(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (n >= 3) {
+            stringBuilder.insert(0, n % 3);
+            n = n / 3;
+        }
+        stringBuilder.insert(0, n);
+
+        StringBuilder reverse = stringBuilder.reverse();
+        int result = 0;
+        for (int i = reverse.length() - 1, j = 0; i >= 0; i--, j++) {
+            result = result + (reverse.charAt(i) - '0') * (int) Math.pow(3, j);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(45));
+        System.out.println(solution(125));
+    }
+}
