1b0b716fdf6c0498b19f68c9baa8e7f246829649
Kimyongjin <yongjin5184@gmail.com>
2020-12-09 20:30:18 +0900

    Programmers 코딩테스트 연습 > 월간 코드 챌린지 시즌1 > 내적

---
+++ src/programmers/DotProduct.java
@@ -0,0 +1,15 @@
+package src.src.programmers;
+
+import java.util.stream.IntStream;
+
+public class DotProduct {
+    public static int solution(int[] a, int[] b) {
+        return IntStream.range(0, a.length).map(index -> a[index] * b[index]).sum();
+    }
+
+    public static void main(String[] args) {
+        int[] a1 = {1, 2, 3, 4};
+        int[] b1 = {-3, -1, 0, 2};
+        System.out.println(solution(a1, b1));
+    }
+}
