3a17ef7f7b3830bea13c83bd7b103ae7955b9ec9
Kimyongjin <yongjin5184@gmail.com>
2020-12-17 01:18:28 +0900

    Leet code lru-cache 풀이

---
+++ src/Leetcode/LRUCache.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+
+    public int get(int key) {
+        return getOrDefault(key, -1);
+    }
+
+    public void set(int key, int value) {
+        put(key, value);
+    }
+}
