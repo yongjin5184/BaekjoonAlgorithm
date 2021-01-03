2a82f4470c89c11384223a72c2a5e2f19a83be9a
Kimyongjin <yongjin5184@gmail.com>
2021-01-04 08:54:09 +0900

    LeetCode 724. Find Pivot Index. Description 추가

--- src/Leetcode/FindPivotIndex.java
+++ src/Leetcode/FindPivotIndex.java
@@ -3,7 +3,12 @@ package Leetcode;
 import java.util.Arrays;
 
 public class FindPivotIndex {
-
+    /**
+     * @description 배열을 받아서 양쪽의 합이 같은 pivot 의 index 를 return 하는 문제
+     * 1. 배열의 합을 구한다.
+     * 2. 하나의 포인터를 두어 배열의 원소를 지정해 가면서 오른쪽 값은 전체 합에서 pivot 값을 빼고, 왼쪽 값은 pivot 값을 더한다.
+     * 3. 왼쪽과 오른쪽이 같아지는 순간 pivot 의 index 를 반환한다.
+     */
     public static int pivotIndex(int[] nums) {
 
         if (nums.length < 3) {
