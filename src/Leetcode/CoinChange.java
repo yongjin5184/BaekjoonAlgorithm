b5baf9903b114c31b47856303c326abf7d76b089
Kimyongjin <yongjin5184@gmail.com>
2021-01-11 09:32:20 +0900

    LeetCode 322. Coin Change 풀이

---
+++ src/Leetcode/CoinChange.java
@@ -0,0 +1,34 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class CoinChange {
+    /**
+     * @description  Coin 의 value 로 amount 를 만들 수 있는 가장 적은 갯수를 구하는 문제
+     * Input: coins = [1,2,5], amount = 11
+     * Output: 3
+     * Explanation: 11 = 5 + 5 + 1
+     */
+    public static int coinChange(int[] coins, int amount) {
+        int MAX = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, MAX);
+        dp[0] = 0;
+        for (int i = 0; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins1 = {1, 2, 5};
+        int[] coins2 = {2};
+        System.out.println(coinChange(coins1, 11));
+        System.out.println(coinChange(coins2, 3));
+    }
+}
