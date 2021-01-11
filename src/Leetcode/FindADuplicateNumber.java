852c099f7b3e6ff0df4f291c9b577b645e2bcf21
Kimyongjin <yongjin5184@gmail.com>
2021-01-11 16:48:14 +0900

    LeetCode 64. Minimum Path Sum 풀이

--- src/Leetcode/FindADuplicateNumber.java
+++ src/Leetcode/FindADuplicateNumber.java
@@ -7,9 +7,9 @@ public class FindADuplicateNumber {
 
     /**
      * @description Array 에서 중복되는 수를 찾아서 return 하는 문제
-     * 1. map 에 값을 넣어서 중복되는 값이 나오면 return 하는 방법
-     * 2. 같은 배열을 만들고 해당 배열의 인덱스 값으로 배열의 값을 옮기면서 해당 인덱스가 차있으면 return 하는 방법
-     * 3. 하나의 배열에서 값을 옮기는데, 옮긴 값은 음수 처리, 음수인 경우 해당 값은 중복되었다고 생각하고 return 하는 방법
+     * 1. map 에 값을 넣어서 중복되는 값이 나오면 return 하는 방법 (Map 사용)
+     * 2. 같은 배열을 만들고 해당 배열의 인덱스 값으로 배열의 값을 옮기면서 해당 인덱스가 차있으면 return 하는 방법 (Array 사용)
+     * 3. 하나의 배열에서 값을 옮기는데, 옮긴 값은 음수 처리, 음수인 경우 해당 값은 중복되었다고 생각하고 return 하는 방법 (기존 Array 사용)
      */
     public static int findDuplicate3(int[] nums) {
         int first = nums[0];
