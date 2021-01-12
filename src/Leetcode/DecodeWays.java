d4d7b083ba3dfeee1395ce6485aeadbbfb6f7759
Kimyongjin <yongjin5184@gmail.com>
2021-01-12 09:45:26 +0900

    LeetCode 91. Decode Ways 풀이

---
+++ src/Leetcode/DecodeWays.java
@@ -0,0 +1,44 @@
+package Leetcode;
+
+public class DecodeWays {
+    /**
+     * @description 문자로된 숫자가 아래와 같이 decoding 이 되어 나타낼 수 있는 문자의 갯수를 구하는 문제
+     * 'A' -> "1"
+     * 'B' -> "2"
+     * ...
+     * 'Z' -> "26"
+     * <p>
+     * ex)
+     * Input: s = "226"
+     * Output: 3
+     * Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+     */
+    public static int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(numDecodings("212325"));
+    }
+}
