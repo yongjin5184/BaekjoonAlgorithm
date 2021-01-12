49ec64a755511f57e352abb5e68ffc5a710f7dd1
Kimyongjin <yongjin5184@gmail.com>
2021-01-13 00:36:04 +0900

    LeetCode 53. Maximum Subarray 패키지 변경

--- src/Leetcode/MaximumSubarray.java
+++ src/Leetcode/MaximumSubarray.java
@@ -1,4 +1,4 @@
-package src.src.Leetcode;
+package Leetcode;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class LeetCode_MaximumSubarray {
+public class MaximumSubarray {
     public static void main(String[] args) throws IOException {
         /**
          * description  : LeetCode Maximum Subarray
