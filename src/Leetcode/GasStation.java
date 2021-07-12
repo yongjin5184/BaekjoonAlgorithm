6c13d960d919ba47634b0698ab5a213f45f9fd20
Kimyongjin <yongjin5184@gmail.com>
2021-07-12 22:30:23 +0900

    134. LeetCode Gas Station 풀이

---
+++ src/Leetcode/GasStation.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+public class GasStation {
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas == null || gas.length == 0) return -1;
+        int sum = 0;
+        int tempSum = 0;
+        int index = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sum += gas[i] - cost[i];
+            tempSum += gas[i] - cost[i];
+            if (tempSum < 0) {
+                tempSum = 0;
+                index = i + 1;
+            }
+        }
+        return sum < 0 ? -1 : index;
+    }
+
+    public static void main(String[] arg) {
+        int i = canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2});
+        System.out.println(i);
+    }
+}
