41d0ec8d4d0d092bffa878f160a86e278574eaa0
Kimyongjin <yongjin5184@gmail.com>
2021-09-20 21:28:13 +0900

    LeetCode 6. ZigZag Conversion 풀이

--- src/ZigzagConversion.java
+++ src/ZigzagConversion.java
@@ -1,5 +1,50 @@
 public class ZigzagConversion {
 
+    /**
+     * @description 6. ZigZag conversion 파라미터로 String 과 row 가 주어졌을때, 아래 예시와 같은  Output 을 반환하는 메소드를 만들어라.
+     * Input
+     * s = PAYPALISHIRING, numRows = 2
+     *
+     * P Y A I H R N
+     * A P L S I I G
+     *
+     * Output
+     * PYAIHRNAPLSIIG
+     *
+     * Input
+     * s = PAYPALISHIRING, numRows = 3
+     *
+     * P   A   H   N
+     * A P L S I I G
+     * Y   I   R
+     *
+     * Output
+     * PAHNAPLSIIGYIR
+     *
+     *
+     * Input
+     * s = PAYPALISHIRING, numRows = 4
+     *
+     * P     I     N
+     * A   L S   I G
+     * Y A   H R
+     * P     I
+     *
+     * Output
+     * PINALSIGYAHRPI
+     *
+     * Input
+     * s = PAYPALISHIRING, numRows = 5
+     *
+     * P       H
+     * A     S I
+     * Y   I   R
+     * P L     I G
+     * A       N
+     *
+     * Output
+     * PHASIYIRPLIGAN
+     */
     public static void main(String[] args) {
         System.out.println(convert("PAYPALISHIRING", 4));
         System.out.println(convert("PAYPALISHIRING", 5));
