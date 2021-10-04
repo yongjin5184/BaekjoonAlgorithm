f5802d06a6e8b7e4a2fb2faf4c7eadf937461a4e
Kimyongjin <yongjin5184@gmail.com>
2021-10-04 22:09:54 +0900

    LeetCode 231. Power of Two 풀이를 추가하라

---
+++ src/Leetcode/PowerOfTwo.java
@@ -0,0 +1,16 @@
+package Leetcode;
+
+public class PowerOfTwo {
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(256));
+        System.out.println(isPowerOfTwo(1/2));
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        return n % 2 == 0 ? isPowerOfTwo(n / 2) : false;
+    }
+}
