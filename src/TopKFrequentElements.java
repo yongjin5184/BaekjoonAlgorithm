d97f151e75258df9c349756d9c89ac97607e48e7
yongjin.kim <yongjin5184@gmail.com>
2022-01-30 07:58:52 +0900

    LeetCode 347. Top K Frequent Elements 풀이를 추가하라

--- src/TopKFrequentElements.java
+++ src/TopKFrequentElements.java
@@ -6,6 +6,14 @@ import java.util.stream.Collectors;
 import static java.util.Map.Entry.comparingByValue;
 
 public class TopKFrequentElements {
+    /**
+     * LeetCode 347. Top K Frequent Elements
+     *
+     * @description 배열에서 가장 빈번하게 나오는 K 개의 숫자를 반환하라
+     * 1. key는 배열의 index 를 갖고 value은 index 의 카운트를 갖는 맵을 만든다.
+     * 2. 맵을 k 만큼 자른 후, sorting 한다.
+     * 3. 맵을 배열로 만든 후 반환한다.
+     */
     public static void main(String[] args) {
         int[] ints = topKFrequent(new int[]{5, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4}, 2);
         for (int num : ints) {
