d552c0ed956b650a9af5b518512069bbc7cae0b8
yongjin.kim <yongjin5184@gmail.com>
2022-05-30 16:52:31 +0900

    Programmers. 숫자 문자열과 영단어 문제를 풀어라 (간략한 버전)

--- src/programmers/NumberAndEnglishWord.java
+++ src/programmers/NumberAndEnglishWord.java
@@ -1,31 +1,6 @@
 package src.src.programmers;
 
-import java.util.Map;
-
-import static java.util.Map.entry;
-
 public class NumberAndEnglishWord {
-
-    static Map<String, String> englishWordsOfThreeSpell = Map.ofEntries(
-            entry("one","1"),
-            entry("two","2"),
-            entry("six","6")
-    );
-
-    static Map<String, String> englishWordsOfFourSpell = Map.ofEntries(
-            entry("zero","0"),
-            entry("four","4"),
-            entry("five","5"),
-            entry("nine","9")
-    );
-
-    static Map<String, String> englishWordsOfFiveSpell = Map.ofEntries(
-            entry("three","3"),
-            entry("seven","7"),
-            entry("eight","8")
-    );
-
-    static int start = 0;
 
     public static void main(String[] arg) {
         System.out.println(solution("one4seveneight"));
@@ -35,41 +10,11 @@ public class NumberAndEnglishWord {
     }
 
     public static int solution(String s) {
-        StringBuilder result = new StringBuilder();
-        int start = 0;
-        while(start < s.length()) {
-            String substring = s.substring(start, start + 1);
-            if (isNumber(substring)) {
-                // 만약 숫자이면
-                result.append(substring);
-                start++;
-                continue;
-            }
+        String[] englishWords = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 
-            // 숫자가 아니면
-            String strNumber;
-            String englishWord;
-            englishWord = s.substring(start, start + 3);
-            if (englishWordsOfThreeSpell.containsKey(englishWord)) {
-                strNumber = englishWordsOfThreeSpell.get(englishWord);
-            } else {
-                englishWord = s.substring(start, start + 4);
-                if (englishWordsOfFourSpell.containsKey(englishWord)) {
-                    strNumber = englishWordsOfFourSpell.get(englishWord);
-                } else {
-                    englishWord = s.substring(start, start + 5);
-                    strNumber = englishWordsOfFiveSpell.get(englishWord);
-                }
-            }
-
-            result.append(strNumber);
-            start += englishWord.length();
+        for (int i = 0; i < 10; i++) {
+            s = s.replaceAll(englishWords[i], String.valueOf(i));
         }
-
-        return Integer.parseInt(result.toString());
-    }
-
-    public static boolean isNumber(String s) {
-        return Character.isDigit(s.charAt(0));
+        return Integer.parseInt(s);
     }
 }
